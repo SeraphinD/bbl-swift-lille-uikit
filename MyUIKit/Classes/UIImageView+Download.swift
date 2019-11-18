721f480d26b2311aa0094910f939e2825b1ef8b4
Séraphin Desumeur <seraphindesumeur@MacBook-Pro-de-Seraphin.local>
2019-11-18 15:41:05 +0100

    uiimageview+download

---
+++ MyUIKit/Classes/UIImageView+Download.swift
@@ -0,0 +1,29 @@
+//
+//  UIImageView+Download.swift
+//  MyUIKit
+//
+//  Created by Séraphin Desumeur on 18/11/2019.
+//
+
+import Foundation
+
+extension UIImageView {
+    func downloaded(from url: URL, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() {
+                self.image = image
+            }
+        }.resume()
+    }
+    func downloaded(from link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: mode)
+    }
+}
